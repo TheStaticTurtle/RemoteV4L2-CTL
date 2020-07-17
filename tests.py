8a222560b6e2a07ab2137dffaf43a6094f1ba272
TheStaticTurtle <tuglersamuel@gmail.com>
2020-07-17 22:56:57 +0200

    Initial commit

---
+++ tests.py
@@ -0,0 +1,17 @@
+from remote_v4l2ctl import utils
+import logging 
+import time
+
+logging.basicConfig(format='[%(asctime)s] [%(name)s]  [%(levelname)s] %(message)s',level=logging.DEBUG )
+logging.warning('This is a Warning')
+
+interpreter = utils.V4L2_CTL()
+
+for i in interpreter.controls:
+    print(i)
+
+while True:
+	for i in range(40,60):
+		interpreter.set_brightness(i)
+		time.sleep(.1)
+#print(interpreter.list_controls())
